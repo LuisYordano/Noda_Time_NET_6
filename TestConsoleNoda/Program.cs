52aff1fecedbbac7a1ce17f3b2b1ff06b764fc62
Luis Yordano <lycruzc@yahoo.com>
2021-11-04 10:18:37 -0500

    Add project files.

---
+++ TestConsoleNoda/Program.cs
@@ -0,0 +1,11 @@
+﻿using NodaTime;
+using NodaTime.Extensions;
+
+LocalDate releaseDateSpot = new LocalDate(2016, 6, 23);//robot spot
+DateTimeZone zone = DateTimeZoneProviders.Tzdb["America/New_York"];
+ZonedClock clock = SystemClock.Instance.InZone(zone);
+LocalDate today = clock.GetCurrentDate();
+Console.WriteLine($"Today's date: {today:yyyy-MM-dd}");
+Period age = Period.Between(releaseDateSpot, today);
+Console.WriteLine(
+    $"Robot Spot is: {age.Years} years, {age.Months} months, {age.Days} days old.");
